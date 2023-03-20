ab41247c0477ff3377fd2243fb690ba61fb247b4
menciatarinupv <menciatarinupv@gmail.com>
2023-03-20 17:56:32 +0100

    segunda actividad creada

---
+++ app/src/main/java/com/example/p_3/otraActividad.java
@@ -0,0 +1,13 @@
+package com.example.p_3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class otraActividad extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
