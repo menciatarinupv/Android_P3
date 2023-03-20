035e728dc5d90b9147350e8c4ec76155a8577571
menciatarinupv <menciatarinupv@gmail.com>
2023-03-20 19:05:36 +0100

    cambia de pantalla

--- app/src/main/java/com/example/p_3/MainActivity.java
+++ app/src/main/java/com/example/p_3/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.p_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView resultado;
+    private static int LA_OTRA_ACTIVIDAD_ID = 13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textoResultado = (TextView) findViewById(R.id.resultado);
     }
+
+        public void botonLlamada_pulsado (View quien){
+            Intent intencion = new Intent(this, otraActividad.class);
+            super.startActivity(intencion);
+        }
+
 }
