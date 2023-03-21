4fc60dcb39805a8ae889e86fdf102b29fb954b48
menciatarinupv <menciatarinupv@gmail.com>
2023-03-21 09:51:49 +0100

    codigo que no se encinde la app

--- app/src/main/java/com/example/p_3/MainActivity.java
+++ app/src/main/java/com/example/p_3/MainActivity.java
@@ -5,22 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private TextView resultado;
     private static int LA_OTRA_ACTIVIDAD_ID = 13;
+    TextView textoResultado = (TextView) findViewById(R.id.resultado);
+    EditText primerNumero = (EditText) findViewById(R.id.primerNumero);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("onCreate", "estoy en onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textoResultado = (TextView) findViewById(R.id.resultado);
     }
 
-        public void botonLlamada_pulsado (View quien){
+    public void botonLlamada_pulsado (View quien){
+            Log.d("botón", "Botón pulsado");
+
             Intent intencion = new Intent(this, otraActividad.class);
             super.startActivity(intencion);
+
+            //convierte los datos en String para pasarlos porque en la otra actividad
+            // recibe con un getString
+            intencion.putExtra("Valor1", "String.valueOf(primerNumero.getText())");
         }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intentRespuesta) {
+        // si acaba la "otra" actividad
+        super.onActivityResult(requestCode, resultCode, intentRespuesta);
+        if (requestCode == 1234) {
+            String respuesta = intentRespuesta.getExtras().getString("suma");
+            this.textoResultado.setText("la suma es " + respuesta);
+        }
+    }
 
 }
--- app/src/main/java/com/example/p_3/otraActividad.java
+++ app/src/main/java/com/example/p_3/otraActividad.java
@@ -1,13 +1,60 @@
 package com.example.p_3;
 
 import android.os.Bundle;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//poner siempre el EXTENDS al crear una nueva clase
 public class otraActividad extends AppCompatActivity {
+private String valor1;
+private int primerNumero;
+private EditText valor2;
+private int segundoNumero;
+private int suma;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        //recibe el String mandado de la actividad 1
+        Log.d("recibe", "recibe string valor 1");
+        Intent origen = getIntent();
+        valor1 = origen.getExtras().getString("Valor1");
+
+        //convertir el string en un valor int
+        Log.d("convertir", "convierte string a int del valor 1");
+        primerNumero = Integer.parseInt(valor1);
+
+        //pasar ID a variables
+        valor2 = (EditText) findViewById(R.id.segundoNumero);
+
     }
+
+
+    public void botonSumar_pulsado (View quien){
+         Log.d("botón", "hacer suma");
+
+        //coger string del campo de texto y convertirlo en int
+        Log.d("convertir2", "conveirte string a int del valor 2");
+        segundoNumero = Integer.parseInt(String.valueOf(valor2.getText()));
+
+        //sumar los int
+        suma = primerNumero + segundoNumero;
+
+        //al volver a pasarlo usar vlueOf para hacer una string de la suma
+        Intent resultadoIntent = new Intent();
+        resultadoIntent.putExtra("suma", "String.valueOf(suma)");
+        setResult(RESULT_OK, resultadoIntent);
+        finish();
+    }
+
+
 }
